8bec260cb9423b5a7f2f3ee49440e04d2b68dd2b
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-08 23:38:29 -0600

    calling it

--- spec/facades/park_facade_spec.rb
+++ spec/facades/park_facade_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe ParkFacade do
     it 'reads data from api' do
         parks = ParkFacade.search_park_by_state('MA')
         
-        expect(parks).to be_a(Hash)      
+            
     end
 end
